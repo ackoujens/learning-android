e95ce53f7f516ffbd462000488664f67a18cdb61
Ackou Jens <ackoujens@gmail.com>
2016-09-04 23:39:58 +0200

    Beautifying code

--- beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/Input.java
+++ beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/Input.java
@@ -15,10 +15,10 @@ public interface Input {
      */
     public static class KeyEvent {
         public static final int KEY_DOWN = 0;
-        public static final int KEY_UP = 1;
+        public static final int KEY_UP   = 1;
 
-        public int type;
-        public int keyCode;
+        public int  type;
+        public int  keyCode;
         public char keyChar;
     }
 
@@ -29,8 +29,8 @@ public interface Input {
      * - pointer ID that was given to a finger
      */
     public static class TouchEvent {
-        public static final int TOUCH_DOWN = 0;
-        public static final int TOUCH_UP = 1;
+        public static final int TOUCH_DOWN    = 0;
+        public static final int TOUCH_UP      = 1;
         public static final int TOUCH_DRAGGED = 2;
 
         public int type;
