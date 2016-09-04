72d362f60fd3bf45c85411d9f80c268de7d3dbd4
Ackou Jens <ackoujens@gmail.com>
2016-09-04 21:08:57 +0200

    Defining basic interfaces for a basic framework. Starting with handling Input

---
+++ beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/Input.java
@@ -0,0 +1,72 @@
+package be.ackoujens.basicgameframework;
+
+import java.util.List;
+
+/**
+ * Created by ackoujens on 04/09/16.
+ */
+public interface Input {
+    /**
+     * Defines constants to encode a KeyEvent type
+     * - type
+     * - key code
+     * - unicode character in case of KEY_UP
+     */
+    public static class KeyEvent {
+        public static final int KEY_DOWN = 0;
+        public static final int KEY_UP = 1;
+
+        public int type;
+        public int keyCode;
+        public char keyChar;
+    }
+
+    /**
+     * Defines constants to encode TouchEvent type
+     * - type
+     * - position of finger relative to UI component origin
+     * - pointer ID that was given to a finger
+     */
+    public static class TouchEvent {
+        public static final int TOUCH_DOWN = 0;
+        public static final int TOUCH_UP = 1;
+        public static final int TOUCH_DRAGGED = 2;
+
+        public int type;
+        public int x, y;
+        public int pointer;
+    }
+
+    // Takes keyCode, returns if corresponding key is pressed or not
+    public boolean isKeyPressed(int keyCode);
+
+    // Takes pointer, returns if corresponding finger is touching or not
+    public boolean isTouchDown(int pointer);
+
+    // Get x-position of active pointer
+    public int getTouchX(int pointer);
+
+    // Get y-position of active pointer
+    public int getTouchY(int pointer);
+
+    // Get acceleration value on the x-axis
+    public float getAccelX();
+
+    // Get acceleration value on the y-axis
+    public float getAccelY();
+
+    // Get acceleration value on the z-axis
+    public float getAccelZ();
+
+    /**
+     * Used for event based handling
+     * - returns KeyEvent & TouchEvent instances that got recorded since last call
+     * - events are ordered according to when they occurred
+     *  - newest event at the end of the list
+     *  - oldest event at the start of the list
+     * @return
+     */
+    public List<KeyEvent> getKeyEvents();
+
+    public List<TouchEvent> getTouchEvents();
+}
