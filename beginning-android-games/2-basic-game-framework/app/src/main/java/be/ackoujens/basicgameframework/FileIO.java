d99b8de7f2f29cdd6c7b2256b60dabf44c4874cc
Ackou Jens <ackoujens@gmail.com>
2016-09-04 21:30:49 +0200

    File IO interface

--- beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/FileIO.java
+++ beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/FileIO.java
@@ -1,8 +1,40 @@
 package be.ackoujens.basicgameframework;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
- * Created by ackoujens on 04/09/16.
+ * FileIO
+ * reading and writing files
+ * packaged files
+ * level files, images, audio files, high scores, game settings, save game states
  */
 public interface FileIO {
+    // Specify a filename and get a stream in return.
+    // Throw an IOException if something goes wrong.
 
+    /**
+     * Assets will be read from the applications APK file
+     * @param filename
+     * @return InputStream
+     * @throws IOException
+     */
+    public InputStream  readAsset(String filename) throws IOException;
+
+    /**
+     * Files will be read from the SD card
+     * @param filename
+     * @return InputStream
+     * @throws IOException
+     */
+    public InputStream  readFile (String filename) throws IOException;
+
+    /**
+     * Files will br written on the SD card
+     * @param filename
+     * @return OutputStream
+     * @throws IOException
+     */
+    public OutputStream writeFile(String filename) throws IOException;
 }
