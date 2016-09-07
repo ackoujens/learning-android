523fdd14e3c07e8e5567e3723ce27dccd9e3d548
Ackou Jens <ackoujens@gmail.com>
2016-09-07 17:49:06 +0200

    Testing android app initial commit
    Contains list without functionality

---
+++ beginning-android-games/3-android-testing/app/src/main/java/be/ackoujens/androidtesting/AndroidTesting.java
@@ -0,0 +1,68 @@
+package be.ackoujens.androidtesting;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * AndroidTesting
+ * an extensive list of small android features
+ */
+public class AndroidTesting extends ListActivity {
+    /**
+     * Listing of all test sub-applications
+     */
+    String tests[] = { "LifeCycle",
+                       "SingleTouch",
+                       "MultiTouch",
+                       "Key",
+                       "Accelerometer",
+                       "Assets",
+                       "ExternalStorage",
+                       "SoundPool",
+                       "MediaPlayer",
+                       "FullScreen",
+                       "RenderView",
+                       "Shape",
+                       "Bitmap",
+                       "Font",
+                       "SurfaceView" };
+
+    /**
+     * Init application and populate it with tests
+     * - Specify list items we want ListActivity to display
+     * @param savedInstanceState
+     */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, tests));
+    }
+
+    /**
+     * When ListItem is clicked
+     * - get item name from tests array
+     * - target class to be executed
+     * - create new intent based on target
+     * - start activity
+     * @param list
+     * @param view
+     * @param position
+     * @param id
+     */
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id) {
+        super.onListItemClick(list, view, position, id);
+        String testName = tests[position];
+        try {
+            Class clazz = Class.forName("be.ackoujens.androidtesting." +testName);
+            Intent intent = new Intent(this, clazz);
+            startActivity(intent);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
