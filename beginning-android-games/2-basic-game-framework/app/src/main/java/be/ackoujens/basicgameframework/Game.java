2ac4505133a5c246d5470423e3216f1ea0a2039d
Ackou Jens <ackoujens@gmail.com>
2016-09-07 15:15:45 +0200

    Abstraction of our game framework

---
+++ beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/Game.java
@@ -0,0 +1,49 @@
+package be.ackoujens.basicgameframework;
+
+/**
+ * Game
+ * hide the complexity for ease of use
+ */
+public interface Game {
+    /**
+     * Access to Input module
+     * @return Input instance
+     */
+    public Input getInput();
+
+    /**
+     * Access to FileIO module
+     * @return FileIO instance
+     */
+    public FileIO getFileIO();
+
+    /**
+     * Access to Graphics module
+     * @return Graphics instance
+     */
+    public Graphics getGraphics();
+
+    /**
+     * Access to Audio module
+     * @return Audio instance
+     */
+    public Audio getAudio();
+
+    /**
+     * Set current screen of the game
+     * @param screen
+     */
+    public void setScreen(Screen screen);
+
+    /**
+     * Returns currently active Screen instance
+     * @return Active Screen
+     */
+    public Screen getCurrentScreen();
+
+    /**
+     * Abstract method to be overridden
+     * @return First screen of game
+     */
+    public Screen getStartScreen();
+}
