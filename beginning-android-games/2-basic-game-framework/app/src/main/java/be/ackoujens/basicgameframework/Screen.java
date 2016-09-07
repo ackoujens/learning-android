b4dcf29e7ebd13b75d32fc9e57c2dae8f7b3e738
Ackou Jens <ackoujens@gmail.com>
2016-09-07 15:20:17 +0200

    Interface of Screen class

---
+++ beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/Screen.java
@@ -0,0 +1,47 @@
+package be.ackoujens.basicgameframework;
+
+/**
+ * Screen
+ * abstract class so we can implement some bookkeeping
+ */
+public abstract class Screen {
+    /**
+     * Core game instance
+     */
+    protected final Game game;
+
+    /**
+     * Create game instance
+     * @param game
+     */
+    public Screen(Game game) {
+        this.game = game;
+    }
+
+    /**
+     * Update screen
+     * @param deltaTime
+     */
+    public abstract void update(float deltaTime);
+
+    /**
+     * Present (updated) screen
+     * @param deltaTime
+     */
+    public abstract void present(float deltaTime);
+
+    /**
+     * Pause the game state
+     */
+    public abstract void pause();
+
+    /**
+     * Resume the game from where it left
+     */
+    public abstract void resume();
+
+    /**
+     * Destroy the game when finished
+     */
+    public abstract void dispose();
+}
