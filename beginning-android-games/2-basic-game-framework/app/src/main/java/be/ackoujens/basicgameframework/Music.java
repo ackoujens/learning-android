34d29e96e74fc6118537a65b62d9f3818cf5e9d3
Ackou Jens <ackoujens@gmail.com>
2016-09-07 14:58:26 +0200

    Altered commenting for documentation

--- beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/Music.java
+++ beginning-android-games/2-basic-game-framework/app/src/main/java/be/ackoujens/basicgameframework/Music.java
@@ -5,24 +5,48 @@ package be.ackoujens.basicgameframework;
  * playback of streamed audio
  */
 public interface Music {
-    // Start playing music stream
+    /**
+     * Start playing music stream
+     */
     public void play();
 
-    // Stop playing music stream
+    /**
+     * Stop playing music stream
+     */
     public void stop();
 
-    // Start from the beginning when music stream reaches the end of the audio file
+    /**
+     * Start from the beginning when music stream reaches the end of the audio file
+     * @param looping
+     */
     public void setLooping(boolean looping);
 
-    // Set volume in float values
+    /**
+     * Set volume in float values
+     * @param volume
+     */
     public void setVolume(float volume);
 
+    /**
+     * Check if audio is playing
+     * @return boolean
+     */
     public boolean isPlaying();
 
+    /**
+     * Check if audio is stopped
+     * @return boolean
+     */
     public boolean isStopped();
 
+    /**
+     * Check if audio is looping
+     * @return boolean
+     */
     public boolean isLooping();
 
-    // Close any system resources related to the Music instance
+    /**
+     * Close any system resources related to the Music instance
+     */
     public void dispose();
 }
