da3a498fccb6696000f1d5c3fe7629f6e79e5c9b
Ackou Jens <ackoujens@gmail.com>
2016-09-14 20:54:36 +0200

    Handling the accelerometer

---
+++ beginning-android-games/4-android-game-framework/app/src/main/java/be/ackoujens/androidgameframework/AccelerometerHandler.java
@@ -0,0 +1,62 @@
+package be.ackoujens.androidgameframework;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ * AccelerometerHandler
+ */
+public class AccelerometerHandler implements SensorEventListener {
+    float accelX;
+    float accelY;
+    float accelZ;
+
+    public AccelerometerHandler(Context context) {
+        SensorManager manager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+        if (manager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
+            Sensor accelerometer = manager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+            manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    /**
+     * Called when there is a new sensor event.  Note that "on changed"
+     * is somewhat of a misnomer, as this will also be called if we have a
+     * new reading from a sensor with the exact same sensor values (but a
+     * newer timestamp).
+     * @param event the {@link SensorEvent SensorEvent}.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        accelX = event.values[0];
+        accelY = event.values[1];
+        accelZ = event.values[2];
+    }
+
+    /**
+     * Called when the accuracy of the registered sensor has changed.  Unlike
+     * onSensorChanged(), this is only called when this accuracy value changes.
+     * @param sensor
+     * @param accuracy The new accuracy of this sensor, one of
+     *                 {@code SensorManager.SENSOR_STATUS_*}
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // nothing to do here
+    }
+
+    public float getAccelX() {
+        return accelX;
+    }
+
+    public float getAccelY() {
+        return accelY;
+    }
+
+    public float getAccelZ() {
+        return accelZ;
+    }
+}
