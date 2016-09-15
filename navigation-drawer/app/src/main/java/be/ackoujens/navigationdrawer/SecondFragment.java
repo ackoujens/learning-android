ae86fc48c6224a5478408c470cdce80ecef5115f
Ackou Jens <ackoujens@gmail.com>
2016-09-15 20:02:47 +0200

    Initial commit for navigation drawer

---
+++ navigation-drawer/app/src/main/java/be/ackoujens/navigationdrawer/SecondFragment.java
@@ -0,0 +1,22 @@
+package be.ackoujens.navigationdrawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ackoujens on 15/09/16.
+ */
+public class SecondFragment extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.second_layout, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
