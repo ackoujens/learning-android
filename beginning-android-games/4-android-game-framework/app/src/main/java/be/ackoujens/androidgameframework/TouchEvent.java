87af5240b8224795f0354e2f449cf0f49bde9f7f
Ackou Jens <ackoujens@gmail.com>
2016-09-16 15:15:32 +0200

    Finished TouchEvent class but class name is marked as an error

---
+++ beginning-android-games/4-android-game-framework/app/src/main/java/be/ackoujens/androidgameframework/TouchEvent.java
@@ -0,0 +1,15 @@
+package be.ackoujens.androidgameframework;
+
+/**
+ * TouchEvent
+ * TODO: figure out what the classname is screaming error
+ */
+public static class TouchEvent {
+    public static final int TOUCH_DOWN = 0;
+    public static final int TOUCH_UP = 1;
+    public static final int TOUCH_DRAGGED = 2;
+
+    public int type;
+    public int x, y;
+    public int pointer;
+}
