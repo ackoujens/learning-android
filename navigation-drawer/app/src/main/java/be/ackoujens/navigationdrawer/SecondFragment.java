08961e7cea4f5dd763d6c62e71d5c2f50a48b9d3
Ackou Jens <ackoujens@gmail.com>
2016-09-16 14:13:27 +0200

    Name in head comment

--- navigation-drawer/app/src/main/java/be/ackoujens/navigationdrawer/SecondFragment.java
+++ navigation-drawer/app/src/main/java/be/ackoujens/navigationdrawer/SecondFragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Created by ackoujens on 15/09/16.
+ * SecondFragment
  */
 public class SecondFragment extends Fragment {
     View myView;
